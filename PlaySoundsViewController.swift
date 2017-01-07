a42a030b88f86c0975de5e72a60da4cc651a7834
Subcipher <krishna@javaworkgroup.com>
2017-01-07 12:34:46 -0500

    completed perfectPitch V1

---
+++ PlaySoundsViewController.swift
@@ -0,0 +1,50 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by kpicart on 1/5/17.
+//  Copyright © 2017 StepwiseDesigns. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+   
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var recordedAudioURL: URL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int { case chipmunk = 0, vader}
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        }
+        configureUI(.playing)
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+       stopAudio()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAudio()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
+    }
+  }
